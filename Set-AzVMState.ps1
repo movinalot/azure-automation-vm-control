874df556059ea7452a254ad6455ef8cd04bf5798
John McDonough <movinalot@gmail.com>
2021-05-17 23:03:09 -0400

    initial commit

---
+++ Set-AzVMState.ps1
@@ -0,0 +1,28 @@
+<#
+    .DESCRIPTION
+        A runbook which starts/stops VMs in my reseouce groups
+
+    .NOTES
+        AUTHOR: jmcdonough@fortinet.com
+        LASTEDIT: May 17, 2021
+#>
+
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("Start","Stop")]
+    [string] $VmAction
+)
+$connectionName = "AzureRunAsConnection"
+
+Connect-AzAccount -Identity
+
+if ($VmAction.Equals("Start")) {
+    Start-AzVM -ResourceGroupName jmcdonough-utility -Name jmcdonough-win10 -NoWait
+    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-eastus2-01 | Start-AzVm -NoWait
+    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-west-us2-01 | Start-AzVm -NoWait
+
+} elseif ($VmAction.Equals("Stop")) {
+    Stop-AzVM -ResourceGroupName jmcdonough-utility -Name jmcdonough-win10 -NoWait -Force
+    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-eastus2-01 | Stop-AzVm -NoWait -Force
+    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-west-us2-01 | Stop-AzVm -NoWait -Force
+}
