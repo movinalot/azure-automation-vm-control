7e6b0f5c92a56e4cc78defd11ac219207693efd5
John McDonough <movinalot@gmail.com>
2021-05-28 15:05:21 -0400

    update resource group names

--- Set-AzVMState.ps1
+++ Set-AzVMState.ps1
@@ -12,17 +12,16 @@ param(
     [ValidateSet("Start","Stop")]
     [string] $VmAction
 )
-$connectionName = "AzureRunAsConnection"
 
 Connect-AzAccount -Identity
 
 if ($VmAction.Equals("Start")) {
     Start-AzVM -ResourceGroupName jmcdonough-utility -Name jmcdonough-win10 -NoWait
     Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-eastus2-01 | Start-AzVm -NoWait
-    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-west-us2-01 | Start-AzVm -NoWait
+    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-westus2-02 | Start-AzVm -NoWait
 
 } elseif ($VmAction.Equals("Stop")) {
     Stop-AzVM -ResourceGroupName jmcdonough-utility -Name jmcdonough-win10 -NoWait -Force
     Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-eastus2-01 | Stop-AzVm -NoWait -Force
-    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-west-us2-01 | Stop-AzVm -NoWait -Force
+    Get-AzVm -ResourceGroupName jmcdonough-ap-elb-ilb-az-westus2-02 | Stop-AzVm -NoWait -Force
 }
